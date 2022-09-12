d3cc55c41874219f70160dbeba8485c2648a14e5
Ayush <58210241+ayush897@users.noreply.github.com>
2022-09-12 18:49:04 +0530

    Update index.js
    - when u end game it show the winner team

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-let scoreHomeEl = document.getElementById("score-home-el")
-let scoreGuestEl = document.getElementById("score-guest-el")
+const hscore = document.querySelector(".h-score");
+const gscore = document.querySelector(".g-score");
+const hbtn1 = document.querySelector(".hbtn1");
+const hbtn2 = document.querySelector(".hbtn2");
+const hbtn3 = document.querySelector(".hbtn3");
+const gbtn1 = document.querySelector(".gbtn1");
+const gbtn2 = document.querySelector(".gbtn2");
+const gbtn3 = document.querySelector(".gbtn3");
+const newBtn = document.querySelector(".new-btn");
+const endBtn = document.querySelector(".end-btn");
+const homeBG = document.querySelector(".home-bg");
+const guestBG = document.querySelector(".guest-bg");
+const message = document.querySelector(".mess");
+const h_btn = document.querySelectorAll(".h-btn");
+const g_btn = document.querySelectorAll(".g-btn");
+let isalive = true;
+let homeScore = 0;
+let guestScore = 0;
 
-let countHome = 0
-let countGuest = 0
-// Home Score Counter
-function freethrowHome() {
-    countHome += 1 // countHome = countHome + 1
-    scoreHomeEl.textContent = countHome
-}
-function midrangeHome() {
-    countHome += 2
-    scoreHomeEl.textContent = countHome
-}
-function longthreeHome() {
-    countHome += 3
-    scoreHomeEl.textContent = countHome
-}
-//  Guest Score Counter
-
-function freethrowGuest() {
-    countGuest += 1
-    scoreGuestEl.textContent = countGuest
-}
-function midrangeGuest() {
-    countGuest += 2
-    scoreGuestEl.textContent = countGuest
-}
-function longthreeGuest() {
-    countGuest += 3
-    scoreGuestEl.textContent = countGuest
-}
+hbtn1.addEventListener("click", function () {
+  if (isalive) {
+    homeScore += 1;
+    hscore.textContent = homeScore;
+  }
+});
+hbtn2.addEventListener("click", function () {
+  if (isalive) {
+    homeScore += 2;
+    hscore.textContent = homeScore;
+  }
+});
+hbtn3.addEventListener("click", function () {
+  if (isalive) {
+    homeScore += 3;
+    hscore.textContent = homeScore;
+  }
+});
